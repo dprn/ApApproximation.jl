6a29f14c60be41f6a738b83f005d802652ca1be6
eval <dario.prn@gmail.com>
2016-06-02 09:30:57 +0200

    Cleaned up `bessel.jl`

--- src/bessel.jl
+++ src/bessel.jl
@@ -8,9 +8,9 @@ function discrete_bessel(N::Integer, α::Real, λ::Real)
     return circshift(fft(x), -1)
 end
 
-function discrete_bessel{N,T<:Real}(x::Frequency{N,T})
-    @assert in(slice(x), [0,N-1])
-    x = Complex128[ exp(im * (x.λ * cos(rotate(x.ω, k)))) for k in 0:N-1 ]
+function discrete_bessel(x::Frequency)
+    @assert in(slice(x), [0,camembert(x)-1])
+    x = Complex128[ exp(im * (λ(x) * cos(rotate(ω(x), k)))) for k in 0:camembert(x)-1 ]
     return circshift(fft(x), -1)
 end
 
@@ -23,13 +23,14 @@ Returns the matrices J^n associated with the BispectralSets.
 ATTENTION: J[n] corresponds to J_{n-1}!
 """
 discrete_bessel_matrix(E::BispectralSet) = discrete_bessel_matrix(E, E)
-function discrete_bessel_matrix{N,T<:Real}(E::BispectralSet{N,T}, F::BispectralSet{N,T}; verbose = true)
+function discrete_bessel_matrix(E::BispectralSet, F::BispectralSet; verbose = true)
+    @assert eltype(E) == eltype(F)
     verbose && (length(E) != length(F)) ? println("Non-square Jn matrices") : nothing
 
-    J = Array(Complex128, N, size(E,1), size(F,1))
+    J = Array(Complex128, camembert(E), size(E,1), size(F,1))
 
     # we store the values in a dict to avoid computing them more than once
-    coeff = Dict{Frequency{N,T},Vector{Complex128}}()
+    coeff = Dict{eltype(E),Vector{Complex128}}()
     for i = 1:size(E,1), j = 1:size(F,1)
         x = composition(E[i], F[j])
         if !haskey(coeff,x)
@@ -54,6 +55,7 @@ end
 
 BesselMatrix(E, x...) = BesselMatrix(E,E, x...)
 function BesselMatrix(E::BispectralSet, F::BispectralSet; weights = nothing)
+    @assert eltype(E) == eltype(F)
     @assert(weights == nothing || length(weights) == size(F,1))
 
     matrix = discrete_bessel_matrix(E,F)
